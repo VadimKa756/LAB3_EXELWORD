053c25025a8bed0090cfbdbb38d768cb03813e80
unknown <429195-5>
2024-01-24 11:05:54 +0300

    LAB3

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.order = new System.Windows.Forms.Label();
+            this.date = new System.Windows.Forms.Label();
+            this.supplier = new System.Windows.Forms.Label();
+            this.Buyer = new System.Windows.Forms.Label();
+            this.textBoxSupplier = new System.Windows.Forms.TextBox();
+            this.textBoxBuyer = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.amount = new System.Windows.Forms.Label();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // order
+            // 
+            this.order.AutoSize = true;
+            this.order.Location = new System.Drawing.Point(747, 15);
+            this.order.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.order.Name = "order";
+            this.order.Size = new System.Drawing.Size(32, 17);
+            this.order.TabIndex = 0;
+            this.order.Text = "108";
+            // 
+            // date
+            // 
+            this.date.AutoSize = true;
+            this.date.Location = new System.Drawing.Point(707, 46);
+            this.date.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(72, 17);
+            this.date.TabIndex = 1;
+            this.date.Text = "12.2.2017";
+            // 
+            // supplier
+            // 
+            this.supplier.AutoSize = true;
+            this.supplier.Location = new System.Drawing.Point(17, 16);
+            this.supplier.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.supplier.Name = "supplier";
+            this.supplier.Size = new System.Drawing.Size(81, 17);
+            this.supplier.TabIndex = 2;
+            this.supplier.Text = "Поставщик";
+            // 
+            // Buyer
+            // 
+            this.Buyer.AutoSize = true;
+            this.Buyer.Location = new System.Drawing.Point(15, 101);
+            this.Buyer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Buyer.Name = "Buyer";
+            this.Buyer.Size = new System.Drawing.Size(86, 17);
+            this.Buyer.TabIndex = 3;
+            this.Buyer.Text = "Покупатель";
+            // 
+            // textBoxSupplier
+            // 
+            this.textBoxSupplier.Location = new System.Drawing.Point(113, 36);
+            this.textBoxSupplier.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxSupplier.Name = "textBoxSupplier";
+            this.textBoxSupplier.Size = new System.Drawing.Size(253, 22);
+            this.textBoxSupplier.TabIndex = 4;
+            // 
+            // textBoxBuyer
+            // 
+            this.textBoxBuyer.Location = new System.Drawing.Point(113, 121);
+            this.textBoxBuyer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxBuyer.Name = "textBoxBuyer";
+            this.textBoxBuyer.Size = new System.Drawing.Size(253, 22);
+            this.textBoxBuyer.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 457);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Итого:";
+            // 
+            // amount
+            // 
+            this.amount.AutoSize = true;
+            this.amount.Location = new System.Drawing.Point(21, 495);
+            this.amount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.amount.Name = "amount";
+            this.amount.Size = new System.Drawing.Size(0, 17);
+            this.amount.TabIndex = 8;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(343, 457);
+            this.generateButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(204, 62);
+            this.generateButton.TabIndex = 9;
+            this.generateButton.Text = "Сформировать отчёт";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 178);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(436, 256);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(672, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Заказ №";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 554);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.amount);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxBuyer);
+            this.Controls.Add(this.textBoxSupplier);
+            this.Controls.Add(this.Buyer);
+            this.Controls.Add(this.supplier);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.order);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Сформировать накладную";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label order;
+        private System.Windows.Forms.Label date;
+        private System.Windows.Forms.Label supplier;
+        private System.Windows.Forms.Label Buyer;
+        private System.Windows.Forms.TextBox textBoxSupplier;
+        private System.Windows.Forms.TextBox textBoxBuyer;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label amount;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+using Microsoft.Office.Interop.Excel;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDataGridView();
+        }
+
+        private void InitializeDataGridView()
+        {
+            // Добавление столбцов в DataGridView
+            dataGridView1.Columns.Add("Number", "№");
+            dataGridView1.Columns.Add("Product", "Товар");
+            dataGridView1.Columns.Add("Quantity", "Количество");
+            dataGridView1.Columns.Add("Price", "Цена");
+
+            // Установка автоматической ширины столбцов
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            // Создание нового документа Word
+            Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
+            Document wordDoc = wordApp.Documents.Add();
+
+            // Создание нового документа Excel
+            Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+            Workbook excelWorkbook = excelApp.Workbooks.Add();
+            Worksheet excelWorksheet = excelWorkbook.Sheets[1];
+
+            try
+            {
+                // Заполнение шапки документа Word
+                wordDoc.Content.Text = "Расходная накладная № " + order.Text + " от " + date.Text + "\n";
+                wordDoc.Content.Text += "Поставщик: " + textBoxSupplier.Text + "\nПокупатель: " + textBoxBuyer.Text + "\n";
+
+                // Создание таблицы в документе Word
+                Table wordTable = wordDoc.Tables.Add(wordDoc.Paragraphs[wordDoc.Paragraphs.Count].Range, dataGridView1.Rows.Count + 1, 5);
+
+                // Заполнение заголовков таблицы в документе Word
+                for (int j = 0; j < 4; j++)
+                {
+                    wordTable.Cell(1, j + 1).Range.Text = dataGridView1.Columns[j].HeaderText;
+                }
+                wordTable.Cell(1, 5).Range.Text = "Сумма";
+
+                // Заполнение данных в таблицу Word
+                double totalSum = 0; // Общая сумма
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        var value = dataGridView1.Rows[i].Cells[j].Value;
+                        wordTable.Cell(i + 2, j + 1).Range.Text = value != null ? value.ToString() : "";
+                    }
+
+                    double quantity = Convert.ToDouble(dataGridView1.Rows[i].Cells[2].Value);
+                    double price = Convert.ToDouble(dataGridView1.Rows[i].Cells[3].Value);
+                    double sum = quantity * price;
+
+                    totalSum += sum; // Добавление текущей суммы к общей сумме
+
+                    wordTable.Cell(i + 2, 5).Range.Text = sum.ToString();
+                }
+
+                // Удаление последней строки в таблице Word
+                int lastRow = wordTable.Rows.Count;
+                wordTable.Rows[lastRow].Delete();
+
+                // Вставка слова "Итог" и расчета общей суммы после таблицы Word
+                wordDoc.Range(wordDoc.Paragraphs[wordDoc.Paragraphs.Count].Range.End - 1).InsertParagraphAfter();
+                wordDoc.Paragraphs[wordDoc.Paragraphs.Count].Range.Text = "Итог: " + totalSum.ToString();
+
+                // Заполнение шапки документа Excel
+                excelWorksheet.Cells[1, 1] = "Расходная накладная № " + order.Text + " от " + date.Text;
+                excelWorksheet.Cells[2, 1] = "Поставщик: " + textBoxSupplier.Text;
+                excelWorksheet.Cells[3, 1] = "Покупатель: " + textBoxBuyer.Text;
+
+                // Создание таблицы в документе Excel
+                for (int j = 0; j < 4; j++)
+                {
+                    excelWorksheet.Cells[5, j + 1] = dataGridView1.Columns[j].HeaderText;
+                }
+                excelWorksheet.Cells[5, 5] = "Сумма";
+
+                // Заполнение шапки документа Excel
+                excelWorksheet.Cells[1, 1] = "Расходная накладная № " + order.Text + " от " + date.Text;
+                excelWorksheet.Cells[2, 1] = "Поставщик: " + textBoxSupplier.Text;
+                excelWorksheet.Cells[3, 1] = "Покупатель: " + textBoxBuyer.Text;
+
+                // Создание таблицы в документе Excel
+                for (int j = 0; j < 4; j++)
+                {
+                    excelWorksheet.Cells[5, j + 1] = dataGridView1.Columns[j].HeaderText;
+                }
+                excelWorksheet.Cells[5, 5] = "Сумма";
+
+                // Заполнение данных в таблицу Excel
+                int excelRowIndex = 6; // Индекс строки в Excel, начиная с 6
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    bool rowHasData = false;
+
+                    for (int j = 0; j < 4; j++)
+                    {
+                        var value = dataGridView1.Rows[i].Cells[j].Value;
+                        excelWorksheet.Cells[excelRowIndex, j + 1] = value != null ? value : "";
+
+                        if (value != null && !string.IsNullOrEmpty(value.ToString()))
+                        {
+                            rowHasData = true;
+                        }
+                    }
+
+                    if (rowHasData)
+                    {
+                        double quantity = Convert.ToDouble(dataGridView1.Rows[i].Cells[2].Value);
+                        double price = Convert.ToDouble(dataGridView1.Rows[i].Cells[3].Value);
+                        double sum = quantity * price;
+
+                        excelWorksheet.Cells[excelRowIndex, 5] = sum;
+                        excelRowIndex++;
+                    }
+                }
+
+                // Добавление "Итог" в соседнюю ячейку после таблицы Excel
+                excelWorksheet.Cells[excelRowIndex, 1] = "Итог:";
+                excelWorksheet.Cells[excelRowIndex, 2] = totalSum;
+
+                
+                // Установка черного цвета контура таблицы в Word
+                wordTable.Borders.Enable = 1;  // 1 = True
+                wordTable.Borders.InsideLineStyle = WdLineStyle.wdLineStyleSingle;
+                wordTable.Borders.OutsideLineStyle = WdLineStyle.wdLineStyleSingle;
+                wordTable.Borders.OutsideColor = WdColor.wdColorBlack;
+                wordTable.Borders.InsideColor = WdColor.wdColorBlack;
+
+                // Установка черного цвета контура таблицы в Excel
+                excelWorksheet.Range["A5", "E" + (excelRowIndex - 1)].Borders.LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
+                excelWorksheet.Range["A5", "E" + (excelRowIndex - 1)].Borders.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Black);
+
+                // Вывод общей суммы в label "amount"
+                amount.Text =  totalSum.ToString("C"); // Форматирование как валюта
+
+                // Сохранение документов
+                string wordFilePath = @"C:\Users\429195-5\Desktop\WordDocument.docx";
+                string excelFilePath = @"C:\Users\429195-5\Desktop\ExcelDocument.xlsx";
+
+                wordDoc.SaveAs2(wordFilePath);
+                excelWorkbook.SaveAs(excelFilePath);
+            }
+            finally
+            {
+                // Закрытие Word и Excel
+                wordApp.Quit();
+                excelApp.Quit();
+
+                // Освобождение ресурсов Word
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(wordDoc);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(wordApp);
+
+                // Освобождение ресурсов Excel
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelWorksheet);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelWorkbook);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+            }
+        }
+    }
+}
